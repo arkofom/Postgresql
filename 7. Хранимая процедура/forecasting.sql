a46a5f4009de908f77c6de4d0d6ef384bb996246
Artem Fomin <artem.fomin@outlook.com>
2020-05-31 22:07:38 +0300

    правильный вариант

--- 7. Хранимая процедура/forecasting.sql
+++ 7. Хранимая процедура/forecasting.sql
@@ -1,9 +1,9 @@
-drop function if exists moving_average(d1 date, d2 date);
+drop function if exists exp_average(d1 date, d2 date, alpha double precision);
 
-create or replace function moving_average(d1 date, d2 date)
+create or replace function exp_average(d1 date, d2 date, alpha double precision)
     returns table
             (
-                client     int,
+                storage    int,
                 goods      int,
                 date       date,
                 volume     int,
@@ -12,38 +12,33 @@ create or replace function moving_average(d1 date, d2 date)
 as
 $$
 declare
-    cursor cursor for
-        select recept.storage                                                      st,
-               recgoods.goods                                                      g,
-               recept.ddate                                                        d,
-               sum(recgoods.volume * goods.length * goods.height * goods.width) as vol
-        from recept
-                 join recgoods on recept.id = recgoods.id
-                 join goods on recgoods.goods = goods.id
-        where recept.ddate >= d1
-          and recept.ddate <= d2
-        group by recept.storage, recgoods.goods, recept.ddate
+    cursor cursor for select recept.storage  st,
+                             recgoods.goods g,
+                             recept.ddate d,
+                             sum(recgoods.volume * goods.length * goods.height * goods.width) as vol
+                      from recept
+                               join recgoods on recept.id = recgoods.id
+                               join goods on recgoods.goods = goods.id
+                      where recept.ddate >= d1
+                        and recept.ddate <= d2
+                      group by recept.storage, recgoods.goods, recept.ddate
+                      order by recept.storage, recgoods.goods, recept.ddate
+
     ;
-    cnt          int := 0;
-    temp_cnt     int := 0;
-    pred         double precision;
-    prev_storage int;
-    prev_goods   int;
+    cnt int := 0;
+    prev_prediction double precision;
+    prev_storage int := -1;
+    prev_goods int := -1;
+
 --
-    storage      int;
-    goods        int;
-    date         date;
-    volume       int;
+    storage int;
+    goods int;
+    date date;
+    vol int;
 begin
-    create temp table tmp
-    (
-        goods_id int,
-        date     date,
-        volume   int
-    );
     create temp table to_return
     (
-        client     int,
+        storage    int,
         goods      int,
         date       date,
         volume     int,
@@ -55,25 +50,15 @@ begin
         exit when not found;
 
         if storage != prev_storage or goods != prev_goods then
-            temp_cnt := 1;
-            truncate tmp;
-            insert into tmp values (goods, date, volume);
-            insert into to_return (client, goods, date, volume, prediction)
-            values (client, goods, date, volume, volume);
+            insert into to_return (storage, goods, date, volume, prediction)
+            values (storage, goods, date, volume, volume);
+            prev_prediction = volume;
         else
-            if temp_cnt < 2 then
-                temp_cnt = temp_cnt + 1;
-                insert into to_return (client, goods, date, volume, prediction)
-                values (client, goods, date, volume, volume);
-                insert into tmp values (goods, date, volume);
-            else
-                temp_cnt = temp_cnt + 1;
-                insert into to_return (client, goods, date, volume, prediction)
-                values (client, goods, date, volume, (select avg(tmp.volume) from tmp));
-                delete from tmp where tmp.date in (select tmp.date from tmp order by tmp.date asc limit 1);
-                insert into tmp values (goods, date, volume);
-            end if;
+            insert into to_return (storage, goods, date, volume, prediction)
+            values (storage, goods, date, volume, volume * alpha + (1 - alpha) * prev_prediction);
+            prev_prediction = volume * alpha + (1 - alpha) * prev_prediction;
         end if;
+
         prev_goods = goods;
         prev_storage = storage;
         cnt := cnt + 1;
@@ -82,9 +67,8 @@ begin
     close cursor;
     return query select * from to_return;
     drop table to_return;
-    drop table tmp;
 end
 $$ language plpgsql;
 
 select *
-from moving_average('2020-02-01', '2020-12-31');
+from exp_average('2020-02-01', '2020-12-31', 0.1);
